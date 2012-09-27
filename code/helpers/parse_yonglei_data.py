8de31ff4aebac58d46fb732af713c5cf68777cc2
andermic <ptolemy777@gmail.com>
2012-09-27 14:39:33 -0700

    Added some comments to python parsing scripts, renamed parse_cluster_data to parse_yonglei_data to avoid confusion

---
+++ code/helpers/parse_yonglei_data.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+from os import listdir
+
+# Get the names of all of the files in the current directory, excepting this code file.
+files = listdir('.')
+files.remove('parse_cluster_results.py')
+
+for i in range(len(files)):
+ # Give the user some progress feedback
+ print '%d/%d' % (i,len(files))
+
+ # Get the contents of a file
+ lines = open(files[i], 'r').readlines()
+ 
+ # Trim first line of metadata
+ lines = lines[1:]
+ 
+ # Trim newline character, split the comma separated values that we need into an array
+ lines = [j.strip().split(',')[5:] for j in lines]
+ 
+ # Get the first class label
+ labels = [lines[0][0]]
+ 
+ # Iterate through each line of data. When a new class label is found, add it to a list of labels.
+ for j in range(1,len(lines)):
+  if lines[j][0] != lines[j-1][0]:
+   labels.append(lines[j][0])
+ 
+ # Don't care what the value of each label actually was, just give each label its own numeric value
+ label_dict = {}
+ for j in range(len(labels)):
+  label_dict[labels[j]] = str(j+1)
+ 
+ # Put each line of the read file back into csv format, with only the necessary columns and labels mapped
+ # to their corresponding numeric values.
+ lines = '\n'.join([label_dict[j[0]]+','+','.join(j[1:]) for j in lines])
+ 
+ # Put the new csv string into a new file
+ open(files[i][:7] + '.csv', 'w').write(lines)
--- results/30hz/kliep_kpre300/parse_cluster.py
+++ results/30hz/kliep_kpre300/parse_cluster.py
@@ -2,14 +2,27 @@
 
 from os import listdir
 
+# Get the names of all of the files in the current directory, excepting this code file.
 file_names = listdir('.')
 file_names.remove('parse_cluster.py')
+
 for i in range(len(file_names)):
+	# Give the user some progress feedback
     print i
-    lines = open(file_names[i],'r').readlines()
-    for j in range(len(lines)):
+	
+	# Get the contents of a file
+	lines = open(file_names[i],'r').readlines()
+    
+	# Find the beginning of the scores
+	for j in range(len(lines)):
         if lines[j][:7] == 'Elapsed':
             score_start = j+1
+	
+	# Trim off some metadata at the end
     scores = lines[score_start:-3]
+	
+	# Get rid of some extra whitespace and the newline character on each line
     scores = [score.strip().replace(' ','') for score in scores]
+	
+	# Put the raw score values into a new csv file
     open('scores' + file_names[i][11:13] + '.csv', 'w').write('\n'.join(scores))
