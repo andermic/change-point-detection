e0d2e8ca4c4dc33252a2bef477f51bd1ebbcc4f1
andermic <ptolemy777@gmail.com>
2012-10-06 11:39:01 -0700

    Started adding comments to cpd code

--- code/source/cc_aggregate.m
+++ code/source/cc_aggregate.m
@@ -1,6 +1,20 @@
 function [ fprs, ag_ars, ag_dts, cc_ars_01, cc_ars_05, cc_ars_1, cc_dts_01, cc_dts_05, cc_dts_1 ] = cc_aggregate(kpre, fpr_split)
 % For each data instance, run a control chart and aggregate results
 
+% Input -  kpre: The number of reference data points used to calculate the
+%                given scores.
+
+% Output - fprs: False positive rates.
+%          ag_ars: Average accuracy rate for each false positive rate.
+%          ad_dts: Average detection time for each false positive rate.
+%          ars_01: All ars for fpr = 0.01.
+%          ars_05: All ars for fpr = 0.05.
+%          ars_1: All ars for fpr = 0.1.
+%          dts_01: All dts for fpr = 0.01.
+%          dts_05: All dts for fpr = 0.05.
+%          dts_1: All dts for fpr = 0.1.
+
+
 addpath('../../../cpd_data');
 load data_names.mat;
 
@@ -29,12 +43,12 @@ for i = 1:size(data_names,1)
     data = csvread(data_names(i,:));
     cc_scores = control_chart(kpre, data);
     
-    % Calculate fpr, ar, and dt for this data instance
+    % Calculate false positive rates, accuracy rates, and detection times for this data instance
     true_changes = changes(data) - kpre;
     [fprs, ars, dts, predictions] = amot_roc(cc_scores, true_changes, kpre, fpr_split);
     fprs = round(fprs*fpr_gran)+1;
     
-    % Store ars and dts for 0.01, 0.05, and 0.1
+    % Store accuracy rates and detection times for 0.01, 0.05, and 0.1 false positive rates
     cc_ars_01 = [cc_ars_01; ars(fprs==2)];
     cc_ars_05 = [cc_ars_05; ars(fprs==6)];
     cc_ars_1 = [cc_ars_1; ars(fprs==11)];
@@ -52,7 +66,7 @@ for i = 1:size(data_names,1)
         ag_dts(fpr) = ag_dts(fpr) / ct_dts(fpr);
     end
 
-    % Write predicted changes for this data instance 
+    % Write predicted changes for this data instance to file
     if fpr_split ~= 0
         csvwrite(strcat(PC_FOLDER_NAME,'/',num2str(fpr_split),'/',data_names(i,:)), predictions);
     end
--- code/source/control_chart.m
+++ code/source/control_chart.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 function[sigma_scores] = control_chart(kpre, data)
 
 accel = data(:,2:4);
@@ -5,9 +7,16 @@ n = size(accel,1);
 sigmas = zeros(n-kpre,3);
 
 for i = 1:n-kpre
+	% Compute the sample mean and standard deviation of the reference data along each
+	% dimension.
     xbar_ref = mean(accel(i:i+kpre-1,:));
     s_ref = std(accel(i:i+kpre-1,:),0);
+
+	% Compute the number of standard deviations from the test data to the reference
+	% data along each dimension.
     sigmas(i,:) = (accel(i+kpre,:)-xbar_ref).^2 ./ s_ref.^2;
 end
 
+% Return the L2 norm (Mahalanobis distance since we assume the 3 normals are
+% independent) of the sigmas along each dimension.
 sigma_scores = sqrt(sum(sigmas,2));
